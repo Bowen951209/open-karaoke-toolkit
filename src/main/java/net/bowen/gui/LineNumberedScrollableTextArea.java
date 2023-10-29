67764e91a541ebf450714f564503c8efa81a2327
bowen951209 <bowen951209@gmail.com>
2023-10-29 08:43:27 +0800

    Line display in text area.

--- src/main/java/net/bowen/gui/LineNumberedScrollableTextArea.java
+++ src/main/java/net/bowen/gui/LineNumberedScrollableTextArea.java
@@ -1,30 +1,75 @@
 package net.bowen.gui;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Element;
 import java.awt.*;
 
 public class LineNumberedScrollableTextArea extends JScrollPane {
+    private final JTextArea textArea, lineHead;
+    private final StringBuilder lineStringBuilder;
+    private int maxLine;
+
 
     // Reference: https://www.tutorialspoint.com/how-can-we-display-the-line-numbers-inside-a-jtextarea-in-java
     public LineNumberedScrollableTextArea(Dimension preferredSize) {
         super();
-        JTextArea textArea = new JTextArea(); // The component where you can type text.
+        textArea = new JTextArea(); // The component where you can type text.
 
 
         setViewportView(textArea); // Wrap the text area with scroll pane, so it becomes scrollable.
         setPreferredSize(preferredSize);
 
         // View line number before each line.
-        JTextArea lineHead = new JTextArea();
+        lineHead = new JTextArea();
         lineHead.setEditable(false);
         lineHead.setBackground(Color.BLACK);
         lineHead.setForeground(Color.WHITE);
-        StringBuilder lineString = new StringBuilder();
-        for (int i = 0; i < 10; i++) {
-            lineString.append(i).append("\n");
-        }
-        lineHead.setText(lineString.toString());
+        lineStringBuilder = new StringBuilder();
+        updateString();
+
+        textArea.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateString();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateString();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updateString();
+            }
+        });
 
         setRowHeaderView(lineHead);
+    }
+
+    private void updateString() {
+        int caretPosition = textArea.getDocument().getLength();
+        Element root = textArea.getDocument().getDefaultRootElement();
+        int currentMaxLine = root.getElementIndex(caretPosition) + 1;
+
+        // if smaller than previous max
+        if (currentMaxLine < maxLine) {
+            lineStringBuilder.delete(
+                    lineStringBuilder.indexOf(String.valueOf(currentMaxLine + 1)),
+                    lineStringBuilder.length()
+            );
+            lineHead.setText(lineStringBuilder.toString());
+            maxLine = currentMaxLine;
+            return;
+        }
+
+        // if bigger than previous max
+        while (currentMaxLine > maxLine) {
+            maxLine++;
+            lineStringBuilder.append(maxLine).append("\n");
+        }
+        lineHead.setText(lineStringBuilder.toString());
     }
 }

cb2b25e2a27fb6c0cbba8d7b8916834fda5bc0d5
bowen951209 <bowen951209@gmail.com>
2024-04-21 07:56:14 +0800

    Added a doc.

--- src/main/java/net/okt/system/LyricsProcessor.java
+++ src/main/java/net/okt/system/LyricsProcessor.java
@@ -24,8 +24,46 @@ public class LyricsProcessor {
         return markTextList.get(markIdx - 1);
     }
 
-    // TODO: write a test
-
+    /**
+     * This method processes the set lyrics and updates the mark-text data to #markTextList according to the karaoke
+     * lyrics rules below:
+     *
+     * <pre>
+     * 1. In general, 2 marks hold a word.
+     * 2. When meeting the symbol “'“, that means it is the link word case, take the chars right before and after “'”.
+     * 3. When meeting a single “\n” symbol, that means it is the end of a line, we want to ignore it and skip to the
+     * next line’s start word - the word after “\n”.
+     * 4. When meeting double “\n”s, that means it is the end of a paragraph, also skip that, but make sure the new
+     * paragraph’s start mark holds “null”, and so that we know it’s not connected to the last mark.
+     *</pre>
+     *
+     * <p>
+     * Then, store the texts corresponding to the marks in a list, where index 0 is the text held by mark0 and mark1,
+     * index 1 is the text held by mark1 and mark2, and so on.
+     * </p>
+     *
+     * Here’s some examples:
+     *<pre>
+     * 1)
+     * Given lyrics: “ab\nc'd\n\nefg”
+     * Marks distribution would be:
+     *
+     * m0 a m1 b m2 cd m3 m4 e m5 f m6 g m7
+     *
+     * The list would be:
+     * {a, b, cd, null, e, f, g}
+     *
+     * 2)
+     *
+     * Given lyrics:  “a\nbc\n\nd\nef'g”
+     *
+     * Marks distribution would be:
+     * m0 a m1 b m2 c m3 m4 d m5 e m6 fg m7
+     *
+     * The list would be:
+     * {a, b, c, null, d, e, fg}
+     * <pre>
+     */
     public void genTextToMarksList() {
         markTextList.clear();
         int charIdx = 0;

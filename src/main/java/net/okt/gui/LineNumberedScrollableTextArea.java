a1b7673ba38e92be20e1a3c3795fd3ddee791eba
bowen951209 <bowen951209@gmail.com>
2024-06-15 16:34:45 +0800

    Added a better row header for LineNumberedScrollableTextArea. Some code rearrangements.

--- src/main/java/net/okt/gui/LineNumberedScrollableTextArea.java
+++ src/main/java/net/okt/gui/LineNumberedScrollableTextArea.java
@@ -3,46 +3,20 @@ package net.okt.gui;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import javax.swing.text.Element;
 import javax.swing.undo.UndoManager;
-import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.HashSet;
 import java.util.Set;
 
 public class LineNumberedScrollableTextArea extends JScrollPane {
-    private final JTextArea textArea, lineHead;
-    private final StringBuilder lineStringBuilder;
+    private final JTextArea textArea;
     private final UndoManager undoManager = new UndoManager();
     private final Set<Runnable> undoListeners = new HashSet<>();
     private final Set<Runnable> redoListeners = new HashSet<>();
 
     private Set<Runnable> documentUpdateCallbacks;
-    private int maxLine;
 
-    public void addUndoListener(Runnable e) {
-        undoListeners.add(e);
-    }
-
-    public void addRedoListener(Runnable e) {
-        redoListeners.add(e);
-    }
-
-    public void setText(String s) {
-        textArea.setText(s);
-    }
-
-    public String getText() {
-        return textArea.getText();
-    }
-
-    public void addDocumentUpdateCallback(Runnable e) {
-        if (documentUpdateCallbacks == null) documentUpdateCallbacks = new HashSet<>();
-        documentUpdateCallbacks.add(e);
-    }
-
-    // Reference: https://www.tutorialspoint.com/how-can-we-display-the-line-numbers-inside-a-jtextarea-in-java
     public LineNumberedScrollableTextArea() {
         super();
         textArea = new JTextArea(); // The component where you can type text.
@@ -50,11 +24,6 @@ public class LineNumberedScrollableTextArea extends JScrollPane {
         setViewportView(textArea); // Wrap the text area with scroll pane, so it becomes scrollable.
 
         // View line number before each line.
-        lineHead = new JTextArea();
-        lineHead.setEditable(false);
-        lineHead.setBackground(Color.BLACK);
-        lineHead.setForeground(Color.WHITE);
-        lineStringBuilder = new StringBuilder();
         documentUpdateCallback();
 
         textArea.getDocument().addDocumentListener(new DocumentListener() {
@@ -94,36 +63,31 @@ public class LineNumberedScrollableTextArea extends JScrollPane {
             }
         });
 
-        setRowHeaderView(lineHead);
+        setRowHeaderView(new TextLineNumber(textArea));
+    }
+
+    public void addUndoListener(Runnable e) {
+        undoListeners.add(e);
+    }
+
+    public void addRedoListener(Runnable e) {
+        redoListeners.add(e);
+    }
+
+    public String getText() {
+        return textArea.getText();
+    }
+
+    public void setText(String s) {
+        textArea.setText(s);
+    }
+
+    public void addDocumentUpdateCallback(Runnable e) {
+        if (documentUpdateCallbacks == null) documentUpdateCallbacks = new HashSet<>();
+        documentUpdateCallbacks.add(e);
     }
 
     private void documentUpdateCallback() {
-        refreshLineHead();
-
         if (documentUpdateCallbacks != null) documentUpdateCallbacks.forEach(Runnable::run);
-    }
-
-    private void refreshLineHead() {
-        int caretPosition = textArea.getDocument().getLength();
-        Element root = textArea.getDocument().getDefaultRootElement();
-        int currentMaxLine = root.getElementIndex(caretPosition) + 1;
-
-        // if smaller than previous max
-        if (currentMaxLine < maxLine) {
-            lineStringBuilder.delete(
-                    lineStringBuilder.indexOf(String.valueOf(currentMaxLine + 1)),
-                    lineStringBuilder.length()
-            );
-            lineHead.setText(lineStringBuilder.toString());
-            maxLine = currentMaxLine;
-            return;
-        }
-
-        // if bigger than previous max
-        while (currentMaxLine > maxLine) {
-            maxLine++;
-            lineStringBuilder.append(maxLine).append("\n");
-        }
-        lineHead.setText(lineStringBuilder.toString());
     }
 }
---
+++ src/main/java/net/okt/gui/TextLineNumber.java
@@ -0,0 +1,318 @@
+package net.okt.gui;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.*;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.HashMap;
+
+
+/**
+ * This class will display line numbers for a related text component. The text
+ * component must use the same line height for each line. TextLineNumber
+ * supports wrapped lines and will highlight the line number of the current
+ * line in the text component.
+ * <p>
+ * This class was designed to be used as a component added to the row header
+ * of a JScrollPane.
+ * <p>
+ * Modified from <a href="https://github.com/tips4java/tips4java/blob/main/source/TextLineNumber.java">here</a>
+ */
+public class TextLineNumber extends JPanel implements CaretListener, DocumentListener {
+    private final static Border OUTER = new MatteBorder(0, 0, 0, 2, Color.GRAY);
+    private final static int HEIGHT = Integer.MAX_VALUE - 1000000;
+    private static  final float DIGIT_ALIGNMENT = 1.0f;
+
+    private final JTextComponent component;
+    private Color currentLineForeground;
+    private int minimumDisplayDigits;
+
+    private int lastDigits;
+    private int lastHeight;
+    private int lastLine;
+    private HashMap<String, FontMetrics> fonts;
+
+    /**
+     * Create a line number component for a text component. This minimum
+     * display width will be based on 3 digits.
+     *
+     * @param component the related text component
+     */
+    public TextLineNumber(JTextComponent component) {
+        this(component, 3);
+    }
+
+    /**
+     * Create a line number component for a text component.
+     *
+     * @param component            the related text component
+     * @param minimumDisplayDigits the number of digits used to calculate
+     *                             the minimum width of the component
+     */
+    public TextLineNumber(JTextComponent component, int minimumDisplayDigits) {
+        this.component = component;
+
+        setFont(component.getFont());
+
+        setBorderGap(5);
+        setCurrentLineForeground(Color.RED);
+        setMinimumDisplayDigits(minimumDisplayDigits);
+
+        component.getDocument().addDocumentListener(this);
+        component.addCaretListener(this);
+    }
+
+    /**
+     * The border gap is used in calculating the left and right insets of the
+     * border. Default value is 5.
+     *
+     * @param borderGap the gap in pixels
+     */
+    public void setBorderGap(int borderGap) {
+        Border inner = new EmptyBorder(0, borderGap, 0, borderGap);
+        setBorder(new CompoundBorder(OUTER, inner));
+        lastDigits = 0;
+        setPreferredWidth();
+    }
+
+    /**
+     * Gets the current line rendering Color
+     *
+     * @return the Color used to render the current line number
+     */
+    public Color getCurrentLineForeground() {
+        return currentLineForeground == null ? getForeground() : currentLineForeground;
+    }
+
+    /**
+     * The Color used to render the current line digits. Default is Color.RED.
+     *
+     * @param currentLineForeground the Color used to render the current line
+     */
+    public void setCurrentLineForeground(Color currentLineForeground) {
+        this.currentLineForeground = currentLineForeground;
+    }
+
+    /**
+     * Specify the minimum number of digits used to calculate the preferred
+     * width of the component. Default is 3.
+     *
+     * @param minimumDisplayDigits the number digits used in the preferred
+     *                             width calculation
+     */
+    public void setMinimumDisplayDigits(int minimumDisplayDigits) {
+        this.minimumDisplayDigits = minimumDisplayDigits;
+        setPreferredWidth();
+    }
+
+    /**
+     * Calculate the width needed to display the maximum line number
+     */
+    private void setPreferredWidth() {
+        Element root = component.getDocument().getDefaultRootElement();
+        int lines = root.getElementCount();
+        int digits = Math.max(String.valueOf(lines).length(), minimumDisplayDigits);
+
+        //  Update sizes when number of digits in the line number changes
+
+        if (lastDigits != digits) {
+            lastDigits = digits;
+            FontMetrics fontMetrics = getFontMetrics(getFont());
+            int width = fontMetrics.charWidth('0') * digits;
+            Insets insets = getInsets();
+            int preferredWidth = insets.left + insets.right + width;
+
+            Dimension d = getPreferredSize();
+            d.setSize(preferredWidth, HEIGHT);
+            setPreferredSize(d);
+            setSize(d);
+        }
+    }
+
+    /**
+     * Draw the line numbers
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        //	Determine the width of the space available to draw the line number
+
+        FontMetrics fontMetrics = component.getFontMetrics(component.getFont());
+        Insets insets = getInsets();
+        int availableWidth = getSize().width - insets.left - insets.right;
+
+        //  Determine the rows to draw within the clipped bounds.
+
+        Rectangle clip = g.getClipBounds();
+        int rowStartOffset = component.viewToModel2D(new Point(0, clip.y));
+        int endOffset = component.viewToModel2D(new Point(0, clip.y + clip.height));
+
+        while (rowStartOffset <= endOffset) {
+            try {
+                if (isCurrentLine(rowStartOffset))
+                    g.setColor(getCurrentLineForeground());
+                else
+                    g.setColor(getForeground());
+
+                //  Get the line number as a string and then determine the
+                //  "X" and "Y" offsets for drawing the string.
+
+                String lineNumber = getTextLineNumber(rowStartOffset);
+                int stringWidth = fontMetrics.stringWidth(lineNumber);
+                int x = getOffsetX(availableWidth, stringWidth) + insets.left;
+                int y = getOffsetY(rowStartOffset, fontMetrics);
+                g.drawString(lineNumber, x, y);
+
+                //  Move to the next row
+
+                rowStartOffset = Utilities.getRowEnd(component, rowStartOffset) + 1;
+            } catch (Exception e) {
+                break;
+            }
+        }
+    }
+
+    /*
+     *  We need to know if the caret is currently positioned on the line we
+     *  are about to paint so the line number can be highlighted.
+     */
+    private boolean isCurrentLine(int rowStartOffset) {
+        int caretPosition = component.getCaretPosition();
+        Element root = component.getDocument().getDefaultRootElement();
+
+        return root.getElementIndex(rowStartOffset) == root.getElementIndex(caretPosition);
+    }
+
+    /*
+     *	Get the line number to be drawn. The empty string will be returned
+     *  when a line of text has wrapped.
+     */
+    protected String getTextLineNumber(int rowStartOffset) {
+        Element root = component.getDocument().getDefaultRootElement();
+        int index = root.getElementIndex(rowStartOffset);
+        Element line = root.getElement(index);
+
+        if (line.getStartOffset() == rowStartOffset)
+            return String.valueOf(index + 1);
+        else
+            return "";
+    }
+
+    /*
+     *  Determine the X offset to properly align the line number when drawn
+     */
+    private int getOffsetX(int availableWidth, int stringWidth) {
+        return (int) ((availableWidth - stringWidth) * DIGIT_ALIGNMENT);
+    }
+
+    /*
+     *  Determine the Y offset for the current row
+     */
+    private int getOffsetY(int rowStartOffset, FontMetrics fontMetrics)
+            throws BadLocationException {
+        //  Get the bounding rectangle of the row
+
+        Rectangle2D r = component.modelToView2D(rowStartOffset);
+        int lineHeight = fontMetrics.getHeight();
+        int y = (int) (r.getY() + r.getHeight());
+        int descent = 0;
+
+        //  The text needs to be positioned above the bottom of the bounding
+        //  rectangle based on the descent of the font(s) contained on the row.
+
+        if (r.getHeight() == lineHeight)  // default font is being used
+        {
+            descent = fontMetrics.getDescent();
+        } else  // We need to check all the attributes for font changes
+        {
+            if (fonts == null)
+                fonts = new HashMap<>();
+
+            Element root = component.getDocument().getDefaultRootElement();
+            int index = root.getElementIndex(rowStartOffset);
+            Element line = root.getElement(index);
+
+            for (int i = 0; i < line.getElementCount(); i++) {
+                Element child = line.getElement(i);
+                AttributeSet as = child.getAttributes();
+                String fontFamily = (String) as.getAttribute(StyleConstants.FontFamily);
+                Integer fontSize = (Integer) as.getAttribute(StyleConstants.FontSize);
+                String key = fontFamily + fontSize;
+
+                FontMetrics fm = fonts.get(key);
+
+                if (fm == null) {
+                    Font font = new Font(fontFamily, Font.PLAIN, fontSize);
+                    fm = component.getFontMetrics(font);
+                    fonts.put(key, fm);
+                }
+
+                descent = Math.max(descent, fm.getDescent());
+            }
+        }
+
+        return y - descent;
+    }
+
+    @Override
+    public void caretUpdate(CaretEvent e) {
+        //  Get the line the caret is positioned on
+
+        int caretPosition = component.getCaretPosition();
+        Element root = component.getDocument().getDefaultRootElement();
+        int currentLine = root.getElementIndex(caretPosition);
+
+        //  Need to repaint so the correct line number can be highlighted
+
+        if (lastLine != currentLine) {
+            getParent().repaint();
+            lastLine = currentLine;
+        }
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        documentChanged();
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        documentChanged();
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        documentChanged();
+    }
+
+    /*
+     *  A document change may affect the number of displayed lines of text.
+     *  Therefore the lines numbers will also change.
+     */
+    private void documentChanged() {
+        //  View of the component has not been updated at the time
+        //  the DocumentEvent is fired
+
+        SwingUtilities.invokeLater(() -> {
+            try {
+                int endPos = component.getDocument().getLength();
+                Rectangle2D rect = component.modelToView2D(endPos);
+
+                if (rect != null && rect.getY() != lastHeight) {
+                    setPreferredWidth();
+                    getParent().repaint();
+                    lastHeight = (int) rect.getY();
+                }
+            } catch (BadLocationException ex) { /* nothing to do */ }
+        });
+    }
+}

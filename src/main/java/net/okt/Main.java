4b2ed9ea86f4a7e4cffa715f4ab632c976d1611f
bowen951209 <bowen951209@gmail.com>
2025-01-28 09:56:13 +0800

    Add a list to manage main frame instances. Program will exit when every window are closed.

--- src/main/java/net/okt/Main.java
+++ src/main/java/net/okt/Main.java
@@ -9,10 +9,18 @@ import net.okt.system.SaveLoadManager;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.dnd.DropTarget;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends JFrame {
     public static final String INIT_FRAME_TITLE = "Open Karaoke Toolkit";
+    /**
+     * The instances of the created main frames. Used to manage multiple instances.
+     */
+    private static final List<Main> instances = new ArrayList<>();
 
     private final SaveLoadManager saveLoadManager;
     private final LyricsProcessor lyricsProcessor;
@@ -23,22 +31,36 @@ public class Main extends JFrame {
     public Main(String title, String propsFile) {
         // Init settings.
         super(title);
+        instances.add(this);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setSize((int) (screenSize.width / 1.7f), (int) (screenSize.height / 1.7f));
 
-        saveLoadManager = new SaveLoadManager(this);
+        // Frame close listener. Exit the program if all instances are closed.
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                instances.remove(Main.this);
+                if (instances.isEmpty()) {
+                    System.out.println("All windows are closed. Exiting...");
+                    System.exit(0);
+                }
+            }
+        });
 
+        saveLoadManager = new SaveLoadManager(this);
         lyricsProcessor = new LyricsProcessor(saveLoadManager);
         viewport = new Viewport(saveLoadManager, lyricsProcessor);
         timeline = new Timeline(saveLoadManager, lyricsProcessor, viewport);
         textArea = getTextArea();
 
+        // Load the project if the props file is provided.
         if (propsFile != null) saveLoadManager.load(new File(propsFile), textArea);
 
         // The drag and drop.
         new DropTarget(this, new FileDropListener(saveLoadManager, textArea));
 
+        // Add components and show after the project is ready.
         addComponents();
     }
 
